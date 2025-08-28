6f64d2d574c8beb5aef0dc1e259aed5c8c89bb1b
zhjihuang <zhjihuang@gmail.com>
2025-08-28 13:52:47 +0000

    Removed unnecessary sudo

--- build_files/build.sh
+++ build_files/build.sh
@@ -23,9 +23,9 @@ set -ouex pipefail
 
 # systemctl enable podman.socket
 
-sudo wget -O /etc/yum.repos.d/linux-surface.repo https://pkg.surfacelinux.com/fedora/linux-surface.repo
+wget -O /etc/yum.repos.d/linux-surface.repo https://pkg.surfacelinux.com/fedora/linux-surface.repo
 
-wget https://github.com/linux-surface/linux-surface/releases/download/silverblue-20201215-1/kernel-20201215-1.x86_64.rpm
+https://github.com/linux-surface/linux-surface/releases/download/silverblue-20201215-1/kernel-20201215-1.x86_64.rpm
 
 rpm-ostree override replace ./*.rpm \
 	--remove kernel-core \
